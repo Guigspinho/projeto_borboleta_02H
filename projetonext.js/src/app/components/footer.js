afe8d47a12addb4502490036780542df2c07e2be
Guigspinho <84827957+Guigspinho@users.noreply.github.com>
2026-04-29 19:43:03 +0000

    imagem footer

--- projetonext.js/src/app/components/footer.js
+++ projetonext.js/src/app/components/footer.js
@@ -1,6 +1,14 @@
 import '../globals.css'
 import Image from 'next/image';
 
+function MembroEquipe({ src, nome }) {
+    return (
+        <figure className="footerfigure">
+            <Image src={src} alt={nome} className="footerimgequipe" width={150} height={150} />
+            <figcaption className="footernomealuno">{nome}</figcaption>
+        </figure>
+    );
+}
 
 export default function Footer() {
     return (
@@ -8,18 +16,9 @@ export default function Footer() {
             <p className="footertexto" style={{ marginBottom: '30px' }}>R. Conselheiro Brotero, 1353 - Santa Cecilia, São Paulo - SP, 01232-011</p>
             <p className="footertexto">Desenvolvido por:</p>
             <section className="footersection">
-                <figure className="footerfigure">
-                    <Image src="/iconeguilherme.png" alt="Imagem de perfil do membro do grupo Guilherme Pinho" className="footerimgequipe" width={150} height={150} />
-                    <figcaption className="footernomealuno">Guilherme Pinho</figcaption>
-                </figure>
-                <figure className="footerfigure">
-                    <Image src="/iconemoabe.png" alt="Imagem de perfil do membro do grupo Moabe Guedes" className="footerimgequipe" width={150} height={150} />
-                    <figcaption className="footernomealuno">Moabe Guedes</figcaption>
-                </figure>
-                <figure className="footerfigure">
-                    <Image src="/iconeryan.png" alt="Imagem de perfil do membro do grupo Ryan Sousa" className="footerimgequipe" width={150} height={150} />
-                    <figcaption className="footernomealuno">Ryan Sousa</figcaption>
-                </figure>
+                <MembroEquipe src="/iconeguilherme.png" nome="Guilherme Pinho" />
+                <MembroEquipe src="/iconemoabe.png" nome="Moabe Guedes" />
+                <MembroEquipe src="/iconeryan.png" nome="Ryan Sousa" />
             </section>
         </footer>
     )
