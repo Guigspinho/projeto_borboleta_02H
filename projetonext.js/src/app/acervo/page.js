12aea864eefb250fbe159f48f3ac024b2c1acb37
Guigspinho <84827957+Guigspinho@users.noreply.github.com>
2026-04-30 00:46:16 +0000

    acervo quase pronto, só falta adicionar mais livros

--- projetonext.js/src/app/acervo/page.js
+++ projetonext.js/src/app/acervo/page.js
@@ -1,12 +1,12 @@
 import TituloInicial from '../components/tituloinicial';
-
+import AcervoLivros from '../components/acervolivro';
 
 export default function Acervo() {
     return (
         <>
             <TituloInicial texto="Nosso Acervo" />
             <main>
-                
+                <AcervoLivros />
             </main>
         </>
     );
--- projetonext.js/src/app/components/acervolivro.js
+++ projetonext.js/src/app/components/acervolivro.js
@@ -11,3 +11,46 @@ function Livro({imgsrc, altlivro, nomelivro, anovolume }) {
         </section>
     );
 }
+
+export default function AcervoLivros() {
+    return (
+        <section class="sectioncardsacervo">
+            <Livro 
+                imgsrc="/livro1.jpg"
+                altlivro="Capa do mangá My Hero Academia volume 42"
+                nomelivro="My Hero Academia volume" 
+                anovolume="Volume 42. 2025" 
+            />
+
+            <Livro 
+                imgsrc="/livro2.jpg"
+                altlivro="Capa do mangá Jujutsu Kaisen volume 1"
+                nomelivro="Jujutsu Kaisen" 
+                anovolume="Volume 1. 2020" 
+            />
+
+            <Livro
+                imgsrc="/livro3.jpg"
+                altlivro="Capa do livro Percy Jackson e o Ladrão de Raios"
+                nomelivro="Percy Jackson e o Ladrão de Raios" 
+                anovolume="Volume 1. 2005" 
+            />
+
+            <Livro
+                imgsrc="/livro4.jpg"
+                altlivro="Capa do livro O Senhor dos Anéis: A Sociedade do Anel"
+                nomelivro="O Senhor dos Anéis: A Sociedade do Anel" 
+                anovolume="Parte 1. 1954" 
+            />
+
+            <Livro 
+                imgsrc="/livro5.jpg"
+                altlivro="Capa do livro O Senhor dos Anéis: As Duas Torres"
+                nomelivro="O Senhor dos Anéis: As Duas Torres" 
+                anovolume="Parte 2. 1954" 
+            />
+
+
+        </section>
+    )
+}
