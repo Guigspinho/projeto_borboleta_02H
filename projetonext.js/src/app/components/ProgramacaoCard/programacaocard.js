0b55ebf6d5292db3279b881bb73a8f3c1674118a
ryan <ryan9821.silva@gmail.com>
2026-05-13 13:48:40 -0300

    colocando module/styles no programação card
    Updated class names to use CSS modules for styling.

--- projetonext.js/src/app/components/ProgramacaoCard/programacaocard.js
+++ projetonext.js/src/app/components/ProgramacaoCard/programacaocard.js
@@ -1,19 +1,20 @@
 import Image from 'next/image';
+import styles from 'ProgramacaoCard.module.css';
 
 function Card({imgsrc, altevento, tipodeevento, nomedoevento, status }) {
     return (
-        <article className="programacaoeventos">
-            <Image src={imgsrc} alt={altevento} className="imgprogramacaoeventos" width={300} height={200} />
-            <h4 className="h4tituloprogramacao"> {tipodeevento} </h4>
-            <h3 className="h3tituloprogramacao"> {nomedoevento} </h3>
-            <p className="ptituloprogramacao"> {status} </p>
+        <article className={styles.programacaoeventos}>
+            <Image src={imgsrc} alt={altevento} className={styles.imgprogramacaoeventos} width={300} height={200} />
+            <h4 className={styles.h4tituloprogramacao}> {tipodeevento} </h4>
+            <h3 className={styles.h3tituloprogramacao}> {nomedoevento} </h3>
+            <p className={styles.ptituloprogramacao}> {status} </p>
         </article>
     );
 }
 
 export default function ProgramacaoCard() {
     return (
-        <section className="grid_programacao">
+        <section className={styles.grid_programacao}>
             <Card 
                 imgsrc="/saraudajuventude.jpg"
                 altevento="Sarau da Juventude"
