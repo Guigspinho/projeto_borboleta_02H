4e703fd4de9e6a133c4bdcdff4c5a1355e3aa6d8
ryan <ryan9821.silva@gmail.com>
2026-05-13 13:41:15 -0300

    colocando module/styles no Menu
    Updated class names to use CSS modules for styling.

--- projetonext.js/src/app/components/Menu/menu.js
+++ projetonext.js/src/app/components/Menu/menu.js
@@ -3,13 +3,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import MudarTema from '../MudarTema/mudartema';
+import styles from './Menu.module.css';
 
 export default function Menu() {
   return (
-    <header className="menu">
+    <header className={styles.menu}>
       <MudarTema />
 
-      <nav className="links">
+      <nav className={styles.links}>
         <Link href="/eventos">EVENTOS</Link> 
         <Link href="/programacao">PROGRAMAÇÃO</Link> 
         <Link href="/acervo">ACERVO</Link> 
