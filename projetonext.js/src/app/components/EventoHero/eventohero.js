2aada1db2b66a6247f5ca42aa9898bc982ac2f84
ryan <ryan9821.silva@gmail.com>
2026-05-14 12:08:01 +0000

    componente evento hero

---
+++ projetonext.js/src/app/components/EventoHero/eventohero.js
@@ -0,0 +1,10 @@
+import styles from './EventoHero.module.css';
+
+export default function EventoHero () {
+    return (
+        <section className={styles.evento-hero}>
+        <h1>Faça o seu evento conosco</h1>
+        <p>Realize seu evento literário, cultural ou educacional em nossa biblioteca. Oferecemos suporte para projetos de lançamento de livro, rodas de leitura, oficinas e encontros com autores.</p>
+    </section>
+    )
+}
